8f9933c8537ba6730a4702a4179ca6cf728a6b34
Julian Aloise <julian.aloise@mail.concordia.ca>
2025-09-09 19:22:28 -0400

    add assets and hamburger icon

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,15 +1,21 @@
-import React from "react";
+import React, {useState} from "react"; //import useState to keep track if hamburger menu is open or not
 import styles from "./NavBar.module.css"; //import css from navbar.module.css
+import { getImageUrl } from "../../utils"; //import function for getting images
 
+//src= "../../../assets/nav/menuIcon.png"
 
 export const NavBar = () => {
+    console.log(getImageUrl("nav/menuIcon.png"))
+    
     //href = "/" returns to top of page when clicked
     return (<nav className= {styles.navbar}>
         <a className = {styles.title} href="/">My Portfolio</a>  
         <div className= {styles.menu}>
+            
+            <img className={styles.menuBtn} src= {getImageUrl("nav/menuIcon.png")} alt="Hamburger Menu Icon"/>
             <ul className = {styles.menuItems}>
                 <li>
-                    <a href="#about">About</a>
+                    <a href="#about">About</a> 
                 </li>
                 <li>
                     <a href="#experience">Experience</a>
