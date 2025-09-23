4bb2f5192ba5f6af6ee585860ad2f69d6920a1a8
Julian Aloise <julian.aloise@mail.concordia.ca>
2025-09-23 01:01:31 -0400

    Started Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import styles from './App.module.css'
 import { NavBar } from './components/NavBar/NavBar'
- 
+import {Hero} from './components/Hero/Hero'
+
 function App() {
 
   return (
     <div className={styles.App}>
       <NavBar/>
+      <Hero/>
     </div>
   )
 }
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Hero = () => {
+    return (
+        <section>
+            {/*Left side*/}
+            <div>
+                <h1>Hi, I am </h1>
+                <p>I am a recent computer engineerin graduate. Welcome to my portfolio!</p>
+            </div>
+            <a href = "mailto:j.aloise2000@gmail.com">Contact Me</a>
+        </section>
+    );
+};
