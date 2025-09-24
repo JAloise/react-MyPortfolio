7e0a9e61fd811293f0a483bf5c88329c6dea070f
Julian Aloise <julian.aloise@mail.concordia.ca>
2025-09-24 00:22:21 -0400

    html for hero set

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 
+import { getImageUrl } from "../../utils";
+import styles from "./Hero.module.css"; //import css from Hero.module.css
+
 export const Hero = () => {
     return (
-        <section>
+        <section className = {styles.container}>
             {/*Left side*/}
-            <div>
-                <h1>Hi, I am </h1>
-                <p>I am a recent computer engineerin graduate. Welcome to my portfolio!</p>
+            <div classname = {styles.content}>
+                <h1 className= {styles.title}>Hi, I am </h1>
+                <p className= {styles.text}>I am a recent computer engineerin graduate. Welcome to my portfolio!</p>
+                <a href = "mailto:j.aloise2000@gmail.com" className= {styles.contactbtn}>Contact Me</a>
             </div>
-            <a href = "mailto:j.aloise2000@gmail.com">Contact Me</a>
+            <img className= {styles.img} src = {getImageUrl("contact/mypic.jpeg")} alt = "My Picture"/>
+            <div className= {styles.topBlur}/>
+            <div className= {styles.bottomBlur}/>
+
         </section>
     );
 };
