552ae501e136b5d4b1a6f45242ef3a60de64e1fb
Julian Aloise <julian.aloise@mail.concordia.ca>
2025-10-11 12:13:55 -0400

    html content of About

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,10 +1,37 @@
 import React from "react";
 
 import styles from "./About.module.css";
-
+import {getImageUrl} from "../../utils";
 
 export const About = () => {
   return (
-    <div>About</div>
+   <section className= {styles.container}>
+    <h2 classname = {styles.title}>About Me</h2>
+    <div>
+        <ul>
+          <li>
+            {/*Image Concordia Logo*/}
+            <div>
+              <h3>Concordia Alumnie</h3>
+              <p>I graduated from Concordia University, in the Computer Engineering CO-OP program</p>
+            </div>
+          </li>
+          <li>
+            {/*Internsip Picture*/}
+            <div>
+              <h3>Experience</h3>
+              <p>I completed four internship throughout my degree</p>
+            </div>
+          </li>
+          <li>
+            {/*Software Picture*/}
+            <div>
+              <h3>Software Development</h3>
+              <p>Passionate about building robust, scalable, and user-focused software systems</p>
+            </div>
+          </li>
+        </ul>
+    </div>
+   </section>
   );
 };
