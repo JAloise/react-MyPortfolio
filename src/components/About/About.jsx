aeba483690d33a538a72740f16bc0215a4ef4932
Julian Aloise <julian.aloise@mail.concordia.ca>
2025-10-13 02:06:53 -0400

    continued About section css

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,33 +5,33 @@ import {getImageUrl} from "../../utils";
 
 export const About = () => {
   return (
-   <section className= {styles.container}>
-    <h2 classname = {styles.title}>About Me</h2>
-    <div>
-        <ul>
-          <li>
+    <section className= {styles.container} id = "about">
+      <h2 className = {styles.title}>About Me</h2>
+      <div className = {styles.content}>
+        <ul className = {styles.items}>
+          <li className = {styles.item}>
             {/*Image Concordia Logo*/}
-            <div>
+            <div className = {styles.aboutItemsText}>
               <h3>Concordia Alumnie</h3>
               <p>I graduated from Concordia University, in the Computer Engineering CO-OP program</p>
             </div>
           </li>
-          <li>
+          <li className = {styles.item}>
             {/*Internsip Picture*/}
-            <div>
+            <div className = {styles.aboutItemsText}>
               <h3>Experience</h3>
               <p>I completed four internship throughout my degree</p>
             </div>
           </li>
-          <li>
+          <li className = {styles.item}>
             {/*Software Picture*/}
-            <div>
+            <div className = {styles.aboutItemsText}>
               <h3>Software Development</h3>
               <p>Passionate about building robust, scalable, and user-focused software systems</p>
             </div>
           </li>
         </ul>
-    </div>
-   </section>
+      </div>
+    </section>
   );
 };
