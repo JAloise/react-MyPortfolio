a22a1dd9f7bde614bf78d0fa041977223ef5aec4
Julian Aloise <julian.aloise@mail.concordia.ca>
2025-10-22 00:20:07 -0400

    experience continue

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
-
+import skills from "../../data/skills.json"
 import styles from "./Experience.module.css";
 import {getImageUrl} from "../../utils";
 
 export const Experience = () => {
     return (
-        <section>
-        <h2 className = {styles.title}>Experience</h2>
+        <section id = "Experience">
+            <h2 className = {styles.title}>Experience</h2>
+            <div>
+                {/*This is left side where all logos are located*/}
+                <div>{
+                    skills.map((skill,id)=> {
+                        return <div key = {id}>
+                            <div>
+                                <img src={getImageUrl(skill.imgsource)} alt= {skill.title}/>
+                            </div>
+                        </div>
+                    })
+                }</div>
+                <ul></ul>
+            </div>
         </section>
     );
 };
+//TODO: Add skill images and experience company logos
+//TODO: look up map function
